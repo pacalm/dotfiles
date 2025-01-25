bd019e8d245d6a0c15f562e5e6e5fe2f5ddf2949
pacalm <indicum8hydrangea@gmail.com>
2025-01-25 15:17:53 +0900

    update: taskwarrior

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -4,3 +4,11 @@ zoxide init fish | source
 fzf_configure_bindings --variables
 
 eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
+
+function task_info
+    task calendar
+    task summary
+    task
+end
+
+task_info
